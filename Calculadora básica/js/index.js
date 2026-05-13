8590aa6b05a96e79b8e6d1115bc5564e650cfe29
Josselinne77 <lopez.henriquez2027@hilasal.superate.org>
2026-05-13 17:33:28 -0600

    Add files via upload

---
+++ Calculadora básica/js/index.js
@@ -0,0 +1,9 @@
+let num1 = Number(prompt("Ingresa el primer número:"));
+let num2 = Number(prompt("Ingresa el segundo número:"));
+
+console.log("Suma:", num1 + num2);
+console.log("Resta:", num1 - num2);
+console.log("Multiplicación:", num1 * num2);
+console.log("División:", num1 / num2);
+console.log("Módulo:", num1 % num2);
+console.log("Potencia:", num1 ** num2);
