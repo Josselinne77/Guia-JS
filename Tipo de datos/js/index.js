2b99ef7d7c064fca54d02e5a2d75da72b750e1aa
Josselinne77 <lopez.henriquez2027@hilasal.superate.org>
2026-05-13 17:23:05 -0600

    Add files via upload

---
+++ Tipo de datos/js/index.js
@@ -0,0 +1,10 @@
+let entero = 10;
+let decimal = 3.14;
+let cadena = "Hola";
+let boleano = true;
+
+console.log(typeof entero);
+console.log(typeof decimal);
+console.log(typeof cadena);
+console.log(typeof boleano);
+
