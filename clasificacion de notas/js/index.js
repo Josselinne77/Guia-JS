6c14637220353347b375706b64a40d3b58f77e9c
Josselinne77 <lopez.henriquez2027@hilasal.superate.org>
2026-05-13 19:15:54 -0600

    Add files via upload

---
+++ clasificacion de notas/js/index.js
@@ -0,0 +1,11 @@
+let nota = Number(prompt("Ingresa la nota:"));
+
+if (nota >= 90) {
+    console.log("A");
+} else if (nota >= 80) {
+    console.log("B");
+} else if (nota >= 70) {
+    console.log("C");
+} else {
+    console.log("Reprobado");
+}
